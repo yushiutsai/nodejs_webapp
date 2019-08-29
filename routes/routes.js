bf6212fd48228b7aaceb6526a9aa57399d5b3414
Brian Tsai <briantsai@gmail.com>
2019-08-29 14:44:04 +0800

    test

--- routes/routes.js
+++ routes/routes.js
@@ -2,31 +2,23 @@ var appRouter = function (app) {
      app.get("/", function (req, res) {
           res.status(200).send({ message: 'Welcome to our restful API' });
      });
-     // app.get("/getItems/:num", function (req, res) {
-     //      var num =req.params.num;
-     //      var data = {
-     //           'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
-     //           'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
-     //           'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg'
-     //      }     
-     //      res.status(200).send(data);
-     // });
-     // app.get("/getText/:txt", function (req, res) {
-     //      var text =req.params.txt;
-     //      var data = {
-     //           'text': text
-     //      }
-     //      res.status(200).send(data);
-     // });
 
-     app.get("/plant/:limits",async function(req,res){ //當連線到Root/ 作出回應
+
+     app.get("/plant/:limits",async function(req,res){ 
           try {
-               const clusterName = "testdpwistron.southeastasia"
+               
+               //parameter init
+               const clusterName = "testdpwistron.southeastasia" // Please input your owner data explorer cluster name
+               const appid = "65bca692-c1e8-4eb5-92a4-7c6feded42d1"
+               const appkey = "appkey"
+               const tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+
+
               // var result = await cacheConnection.hgetallAsync(req.params.name)
               const KustoClient = require("azure-kusto-data").Client;
               const KustoConnectionStringBuilder = require("azure-kusto-data").KustoConnectionStringBuilder;
-              // const kcsb = KustoConnectionStringBuilder.withAadApplicationKeyAuthentication(`https://${clusterName}.kusto.windows.net`,'appid','appkey','authorityId');
-              const kcsb = KustoConnectionStringBuilder.withAadApplicationKeyAuthentication(`https://${clusterName}.kusto.windows.net`,'65bca692-c1e8-4eb5-92a4-7c6feded42d1','b8ec2562-dbcc-433e-a439-01fc39e8e39b','72f988bf-86f1-41af-91ab-2d7cd011db47');
+              // const kcsb = KustoConnectionStringBuilder.withAadApplicationKeyAuthentication(`https://${clusterName}.kusto.windows.net`,'appid','appkey','authorityId'); //authorityId is tenant id
+              const kcsb = KustoConnectionStringBuilder.withAadApplicationKeyAuthentication(`https://${clusterName}.kusto.windows.net`,appid,appkey,tenantId);
       
               const client = new KustoClient(kcsb);
       
